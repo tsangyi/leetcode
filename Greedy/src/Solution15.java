5b2349872ecb13bcc880ca451373ed8f6a80e4e5
tsangyi <157486478@qq.com>
2022-10-01 02:49:50 +0800

    双指针

---
+++ Greedy/src/Solution15.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+/**
+ * @author tsangyi
+ */
+public class Solution15 {
+
+    public static void main(String[] args) {
+
+        int[] ints = {-1, 0, 1, 2, -1, -4};
+        threeSum(ints);
+
+
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        return null;
+    }
+}
