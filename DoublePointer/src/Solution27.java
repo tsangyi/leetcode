f53d5ee939e55817594c1b5d3fb69b91078b009e
tsangyi <157486478@qq.com>
2022-10-06 15:43:08 +0800

    双指针 i移动元素

--- DoublePointer/src/Solution27.java
+++ DoublePointer/src/Solution27.java
@@ -10,9 +10,13 @@ public class Solution27 {
 
     public static void main(String[] args) {
 
+        int[] num = {0,1,2,2,3,0,4,2};
+        int i = removeElement(num, 2);
+        System.out.println(i);
+
     }
 
-    public int removeElement(int[] nums, int val) {
+    public static int removeElement(int[] nums, int val) {
 
         if (nums == null || nums.length == 0) {
             return 0;
